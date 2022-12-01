c353f4a472155bd41de82b4306c435a43cbe6d32
lacriment <lacriment@gmail.com>
2022-12-01 16:36:24 +0300

    day 1

---
+++ day1/solution.py
@@ -0,0 +1,9 @@
+file_content = ""
+with open("input.txt") as f:
+	file_content = f.read()
+
+elves = [x.split('\n') for x in file_content.split('\n\n')]
+elves_total = sorted([sum([int(y) for y in x]) for x in elves], reverse=True)
+
+print(f"Max: {elves_total[0]}")
+print(f"Top 3 sum: {sum(elves_total[0:3])}")
